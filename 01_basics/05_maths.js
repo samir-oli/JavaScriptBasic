39855fc836f210c2469b86fcd534ed43796dfad4
Samir-Oli <hemrajoli1001@gmail.com>
2024-04-17 13:56:35 +0545

    date and time in js

---
+++ 01_basics/05_maths.js
@@ -0,0 +1,32 @@
+// const score = 400
+// const balance = new Number(111.230)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 23.9876
+// console.log(otherNumber.toPrecision(3));
+
+// const hundred = 1000000
+// console.log(hundred.toString().length.toLocaleString('en-US'));
+ 
+// // +++++++++++ Maths +++++++++
+console.log(Math)
+console.log(Math.abs(-789))
+
+console.log(Math.round(4.879))
+console.log(Math.floor(4.879))
+
+console.log(Math.sqrt(4))
+
+// Functions are available at mdn checkout there
+console.log(Math.random()) // 0-1
+
+console.log(math.flooor(Math.random()*10 )+ 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
