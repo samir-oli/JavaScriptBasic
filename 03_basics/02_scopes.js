0996288ed388fe91a41986bb82b34f719dd7742f
Samir-Oli <hemrajoli1001@gmail.com>
2024-04-24 15:50:47 +0545

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,6 +5,7 @@
 // console.log(b)
 // console.log(c)
 // var c = 300 
+{} // scope is just curly braces
 
 let a = 300
 if (true) {
@@ -16,3 +17,29 @@ console.log("Inner: ", a)
 console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one(){
+    const username = "samir"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+
+// ++++++++++++++++++++Interesting+++++++++++++++++
+
+function addOne(value){
+    return value + 1
+    addOne(5)
+
+}
+
+const addTwo = function(value){ // way or writing functions
+    return value + 2
+    addTwo(5)
+}
